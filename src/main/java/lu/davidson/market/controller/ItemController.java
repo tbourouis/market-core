cde0dde4801ea1ecde6afc062db768d1d06f3885
taoufikbourouis <taoufik.bourouiss@gmail.com>
2021-11-12 08:18:30 +0100

    remove unused db setup

--- src/main/java/lu/davidson/market/controller/ItemController.java
+++ src/main/java/lu/davidson/market/controller/ItemController.java
@@ -4,6 +4,7 @@ import lu.davidson.market.json.Item;
 import lu.davidson.market.json.Price;
 import lu.davidson.market.service.ItemService;
 import lu.davidson.market.service.PriceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,17 +13,16 @@ import java.util.List;
 @RequestMapping("/core/items")
 public class ItemController {
 
-    private final ItemService itemService;
-    private final PriceService priceService;
+    @Autowired
+    private ItemService itemService;
+    @Autowired
+    private PriceService priceService;
 
-    public ItemController(ItemService itemService, PriceService priceService) {
-        this.itemService = itemService;
-        this.priceService = priceService;
-    }
     @GetMapping
     public List<Item> getItemList()  {
         return itemService.findAllItems();
     }
+
     @GetMapping("/{itemRef}")
     public Item itemInfo(@PathVariable("itemRef") String reference)  {
         return itemService.findItem(reference);
